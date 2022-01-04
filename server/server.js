9f12cc813b57e2820fa1435c0197cf375f6329b8
Jacob Gasper <jacobgasper@gmail.com>
2022-01-04 13:32:10 -0500

    finished mutations/queriries.js, working on getting code working locally,then deployed on heroku

--- server/server.js
+++ server/server.js
@@ -1,47 +1,49 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+//import apollo server
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas');
+//db connection
+const db = require('./config/connection');
+
+// const routes = require('./routes');
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-})
+});
 
-server.applyMiddleware({app});
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// app.use(routes);
+
+//get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-
-
-app.use(routes); 
-
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
   });
-});
-
-db.on("error", (err) => {
-  console.error("MongoDB connection error: ", err);
 });
